806934d9ff5606623f79b3bac37ea62f98b57c4b
Markmutisya <mark.mwangi@dtudent.moringaschool.com>
2022-06-06 21:06:00 +0300

    Review Strings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'race Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, ${currentUser.slice(0, 1)}!";
